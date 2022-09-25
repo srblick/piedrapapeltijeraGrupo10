b1e5d75f9bb56a01e8b064b8d06e4e440fcbe215
NahuelALB <rocketcs95@gmail.com>
2022-09-24 21:36:40 -0300

    Cambios respecto a los botones y la carpeta components - NahuelAlbarado

--- src/components/MenuInicio.js
+++ src/components/MenuInicio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import imagen from "./images/Principal.jpg";
 import estilo from "./styles/Menu.module.css";
+
 export function MenuInicio(){
 
     return(
@@ -11,8 +12,9 @@ export function MenuInicio(){
         </header>
             <main>
                 <img className={estilo.imagenEdit}src={imagen} alt="imagen principal"/>
-                <button className={estilo.boton}>Empezar a jugar</button>
-                <button className={estilo.boton}>Desarrolladores</button>
+                <button className={estilo.boton} onClick="/juego">Empezar a jugar</button>
+                <button className={estilo.boton} onClick="/reglasdeljuego">Reglas del Juego</button>
+                <button className={estilo.boton} onClick="/desarrolladores">Desarrolladores</button>
             </main>
         <footer className={estilo.tamanio}>
             <h3 className="Pie">Diviértete!!</h3>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { MenuInicio } from "./componentes/MenuInicio";
+import { MenuInicio } from "./components/MenuInicio";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
